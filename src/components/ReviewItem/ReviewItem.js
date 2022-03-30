4361fc80bf81c3df8ab5da933ec7388e77954dfa
Hossen <mdmitho199911@gmail.com>
2022-03-31 00:14:58 +0600

    review item apply and remove item handler

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import './ReviewItem.css'
 
 const ReviewItem = (props) => {
-    const {name, price,img, shipping , quantity} = props.product
+    const {product,handleRemoveProduct} = props
+    const {name, price,img, shipping , quantity} = product
     return (
         <div className='review-item'>
            <div className="">
@@ -13,12 +16,17 @@ const ReviewItem = (props) => {
               <p className='product-name' title={name}>
                   {name.length > 20 ? name.slice(0,20) + '...': name}
                   </p>
-              <p>Price : <span className='orange-color'>{price}</span></p>
-              <p><small>Shipping : {shipping}</small></p>
+              <p>Price : <span className='orange-color'>${price}</span></p>
+              <p><small>Shipping : ${shipping}</small></p>
               <p><small>Quantity : {quantity}</small></p>
               </div>
               <div className="delete-container">
-                  <button>Delete</button>
+                  <button onClick={()=>handleRemoveProduct(product)} className='delete-button'>
+                      <FontAwesomeIcon 
+                      className='delete-icon'
+                      icon={faTrashAlt} 
+                      ></FontAwesomeIcon>
+                  </button>
               </div>
            </div>
         </div>
