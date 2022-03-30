19dc10c48600f9e4d77f1fd4d198b8f25b32beff
Hossen <mdmitho199911@gmail.com>
2022-03-30 21:25:59 +0600

    router setop and intro review page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
 import Header from './components/Header/Header';
 import Shop from './components/Header/Shop/Shop';
 import Inventory from './components/Inventory/Inventory';
 import Orders from './components/Orders/Orders';
 
-
+ 
 function App() {
   return (
     <div className="">
@@ -16,6 +17,7 @@ function App() {
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/orders' element={<Orders></Orders>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
       </Routes>
      
     </div>
